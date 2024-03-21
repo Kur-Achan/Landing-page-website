6f9dd7754b19d7454b5d78f08687007b9f7ce06e
Kur-Achan <you@example.com>
2024-03-21 17:13:16 +0300

    added files

---
+++ arrays.js
@@ -0,0 +1,31 @@
+//Find the last element of the following arrays.arr1 = [3,7,34,90,12]arr2 = [true,"green,where,12,56"]
+
+let arr1 = [3,7,34,90,12];
+let arr2 = [true,"green","where",12,56]
+let lastElementArr1 = arr1[arr1.length - 1];
+
+let lastElementArr2 = arr2[arr2.length - 1];
+
+console.log("Last element of arr1:",lastElementArr1);
+console.log("Last element of arr2:",lastElementArr2);
+
+//write a js program that will join the following array into a stringmypets = ["cow,"Bird","Snake","Dog"];
+
+let mypets = ["Cow","Bird","Snake","Dog"];
+let joinString=mypets.join("Cow","Bird","Snake","Dog");
+console.log(joineString);
+
+
+//Write a JS script to sort the following array itemsvar arr3 = [ -5, 9, 5, 3, 2, -3, 6, 8, 4, 1 ];l
+
+
+
+ var arr3 = [-5,9,5,3,2,-3,6,8,4,1];
+ arr3.sort((a,b)=>a-b);
+
+console.log(arr3);
+
+//Write a program to remove duplicates from the following array.Console the array without duplicates, and console another array that only contains the duplicates
+let arr = ["boy", "man", "girl",  
+          "school", "girl", "woman"];
+          
